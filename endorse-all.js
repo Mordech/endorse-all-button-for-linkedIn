d24959c319a0840740eb29bc41bcfca7a284d763
Elad Mordechai Mizrahi <elad@mizrahi.cc>
2021-08-13 03:26:14 +0300

    ♻️ refactor: clean script

--- endorse-all.js
+++ endorse-all.js
@@ -1,36 +1,60 @@
+// @ts-check
 (function () {
+	// Debug
+	let debug = true;
+
+	/**
+	 * @function
+	 * Use this function to find long selectors using only part of their name
+	 * @param {string} word
+	 * @returns {string}
+	 */
 	function contains(word) {
 		return `*[class*="${word}"]`;
 	}
+	class EndorseAllButton {
+		/**
+		 * @param {HTMLButtonElement} element
+		 * @param {Boolean} isClicked
+		 */
+		constructor(element = document.createElement('button'), isClicked = false) {
+			this.element = element;
+			this.isClicked = isClicked;
+			this.element.id = 'endorse-all';
+			this.element.classList.add(
+				'message-anywhere-button',
+				'pvs-profile-actions__action',
+				'artdeco-button'
+			);
+			this.element.textContent = 'Endorse all';
+			this.element.style.backgroundColor = 'rgb(5, 118, 66)';
+			this.element.onclick = () => this.endorseAll();
+		}
+		toggleClicked() {
+			this.isClicked = true;
+		}
+		endorseAll() {
+			/** @type {HTMLButtonElement} */
+			let additionalSkillsButton = document.querySelector(
+				contains('additional-skills')
+			);
 
-	const body = document.querySelectorAll('body')[0];
+			additionalSkillsButton.getAttribute('aria-expanded') === 'false' &&
+				additionalSkillsButton.click();
 
-	const endorseAllButton = document.createElement('button');
-	endorseAllButton.id = 'endorse-all';
-	endorseAllButton.classList.add('message-anywhere-button');
-	endorseAllButton.classList.add('pvs-profile-actions__action');
-	endorseAllButton.classList.add('artdeco-button');
-	endorseAllButton.textContent = 'Endorse all';
-	endorseAllButton.style.backgroundColor = 'rgb(5, 118, 66)';
-	endorseAllButton.onclick = () => endorseAll();
-	let endorseButtonClick = false;
+			/** @type {NodeListOf<HTMLButtonElement>} */
+			const endorseButtons = document.querySelectorAll(contains('endorse'));
 
-	function endorseAll() {
-		const additionalSkillsButton = document.querySelector(
-			contains('additional-skills')
-		);
+			endorseButtons.forEach(
+				(button) =>
+					button.getAttribute('aria-pressed') === 'false' && button.click()
+			);
 
-		additionalSkillsButton.getAttribute('aria-expanded') === 'false' &&
-			additionalSkillsButton.click();
-
-		const endorseButtons = document.querySelectorAll(contains('endorse'));
-
-		endorseButtons.forEach(
-			(item) => item.getAttribute('aria-pressed') === 'false' && item.click()
-		);
-
-		endorseButtonClick = true;
+			this.toggleClicked();
+		}
 	}
+
+	const endorseAllButton = new EndorseAllButton();
 
 	const callback = function (mutationsList) {
 		for (const mutation of mutationsList) {
@@ -44,14 +68,15 @@
 				) {
 					document
 						.querySelectorAll(contains('skill-categories'))[0]
-						.firstChild.appendChild(endorseAllButton);
-					console.log('endorseAllButton created.');
+						.firstChild.appendChild(endorseAllButton.element);
+					console.log('Endorse-all button created', endorseAllButton.element);
 					break;
 				}
 				document.querySelector(contains('hoverable-content__close-btn')) &&
-					endorseButtonClick &&
+					endorseAllButton.isClicked &&
 					document
 						.querySelector(contains('hoverable-content__close-btn'))
+						// @ts-ignore
 						.click();
 				break;
 			}
@@ -64,6 +89,8 @@
 		subtree: true,
 	};
 
+	const body = document.querySelector('body');
 	const observer = new MutationObserver(callback);
+
 	observer.observe(body, config);
 })();
